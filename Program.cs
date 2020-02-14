6335dcd5017591053e635265895770b81f2ce1c4
PawelPabianczyk <pawel.pabianczyk1999@gmail.com>
2020-02-14 14:30:08 +0100

    Add new files

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kata
+{
+    class Program
+    {
+        //Square Every Digit 7kyu
+        //public static int SquareDigits(int n)
+        //{
+        //    var number = n.ToString();
+        //    string result = "";
+        //    foreach (var x in number)
+        //        result += (Math.Pow((int)Char.GetNumericValue(x),2)).ToString();
+        //    return Int32.Parse(result);
+        //}
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
